94bd2b327484a4daabb8e0198ac3766bf35c0625
Maciej Rurarz <rurarz@drabpol.pl>
2019-03-18 13:14:45 +0100

    test data

--- app/src/main/java/com/example/mytestapp/MainActivity.java
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -17,14 +17,14 @@ import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab = findViewById(R.id.fab);
-    ImageButton btn0st = (ImageButton)(findViewById(R.id.truck));
-    final ImageButton btn1st = (ImageButton)(findViewById(R.id.first_platform));
-    ImageButton btn2nd = (ImageButton)(findViewById(R.id.second_platform));
-    ImageButton btn3rd = (ImageButton)(findViewById(R.id.third_platform));
-    ImageButton btn4th = (ImageButton)(findViewById(R.id.fourth_platform));
-    ImageButton btn5th = (ImageButton)(findViewById(R.id.fifth_platform));
-    ImageButton[] btnwheels = {(ImageButton)(findViewById(R.id.tyre00)),(ImageButton)(findViewById(R.id.tyre01)),(ImageButton)(findViewById(R.id.tyre02)),(ImageButton)(findViewById(R.id.tyre03)),(ImageButton)(findViewById(R.id.tyre04)),(ImageButton)(findViewById(R.id.tyre05)),(ImageButton)(findViewById(R.id.tyre06)),(ImageButton)(findViewById(R.id.tyre07)),(ImageButton)(findViewById(R.id.tyre08)),(ImageButton)(findViewById(R.id.tyre09)),(ImageButton)(findViewById(R.id.tyre10)),(ImageButton)(findViewById(R.id.tyre11)),(ImageButton)(findViewById(R.id.tyre12)),(ImageButton)(findViewById(R.id.tyre13)),(ImageButton)(findViewById(R.id.tyre14)),(ImageButton)(findViewById(R.id.tyre15)),(ImageButton)(findViewById(R.id.tyre16)),(ImageButton)(findViewById(R.id.tyre17)),(ImageButton)(findViewById(R.id.tyre18)),(ImageButton)(findViewById(R.id.tyre19)),(ImageButton)(findViewById(R.id.tyre20)),(ImageButton)(findViewById(R.id.tyre21)),(ImageButton)(findViewById(R.id.tyre22)),(ImageButton)(findViewById(R.id.tyre23)),(ImageButton)(findViewById(R.id.tyre24)),(ImageButton)(findViewById(R.id.tyre25)),(ImageButton)(findViewById(R.id.tyre26)),(ImageButton)(findViewById(R.id.tyre27)),(ImageButton)(findViewById(R.id.tyre28)),(ImageButton)(findViewById(R.id.tyre29)),(ImageButton)(findViewById(R.id.tyre30)),(ImageButton)(findViewById(R.id.tyre31)),(ImageButton)(findViewById(R.id.tyre32)),(ImageButton)(findViewById(R.id.tyre33)),(ImageButton)(findViewById(R.id.tyre34)),(ImageButton)(findViewById(R.id.tyre35)),(ImageButton)(findViewById(R.id.tyre36)),(ImageButton)(findViewById(R.id.tyre37)),(ImageButton)(findViewById(R.id.tyre38)),(ImageButton)(findViewById(R.id.tyre39)),(ImageButton)(findViewById(R.id.tyre40)),(ImageButton)(findViewById(R.id.tyre41)),(ImageButton)(findViewById(R.id.tyre42)),(ImageButton)(findViewById(R.id.tyre43)),(ImageButton)(findViewById(R.id.tyre44)),(ImageButton)(findViewById(R.id.tyre45)),(ImageButton)(findViewById(R.id.tyre46)),(ImageButton)(findViewById(R.id.tyre47)),(ImageButton)(findViewById(R.id.tyre48)),(ImageButton)(findViewById(R.id.tyre49)),(ImageButton)(findViewById(R.id.tyre50)),(ImageButton)(findViewById(R.id.tyre51)),(ImageButton)(findViewById(R.id.tyre52)),(ImageButton)(findViewById(R.id.tyre53)),(ImageButton)(findViewById(R.id.tyre54)),(ImageButton)(findViewById(R.id.tyre55)),(ImageButton)(findViewById(R.id.tyre56)),(ImageButton)(findViewById(R.id.tyre57)),(ImageButton)(findViewById(R.id.tyre58)),(ImageButton)(findViewById(R.id.tyre59)),(ImageButton)(findViewById(R.id.tyre60)),(ImageButton)(findViewById(R.id.tyre61)),(ImageButton)(findViewById(R.id.tyre62)),(ImageButton)(findViewById(R.id.tyre63))};
-    public TableLayout wheels_table = (TableLayout)(findViewById(R.id.wheels_table));
+    ImageButton btn0st = (findViewById(R.id.truck));
+    final ImageButton btn1st = (findViewById(R.id.first_platform));
+    ImageButton btn2nd = (findViewById(R.id.second_platform));
+    ImageButton btn3rd = (findViewById(R.id.third_platform));
+    ImageButton btn4th = (findViewById(R.id.fourth_platform));
+    ImageButton btn5th = (findViewById(R.id.fifth_platform));
+    ImageButton[] btnwheels = {(findViewById(R.id.tyre00)),(findViewById(R.id.tyre01)),(findViewById(R.id.tyre02)),(findViewById(R.id.tyre03)),(findViewById(R.id.tyre04)),(findViewById(R.id.tyre05)),(findViewById(R.id.tyre06)),(findViewById(R.id.tyre07)),(findViewById(R.id.tyre08)),(findViewById(R.id.tyre09)),(findViewById(R.id.tyre10)),(findViewById(R.id.tyre11)),(findViewById(R.id.tyre12)),(findViewById(R.id.tyre13)),(findViewById(R.id.tyre14)),(findViewById(R.id.tyre15)),(findViewById(R.id.tyre16)),(findViewById(R.id.tyre17)),(findViewById(R.id.tyre18)),(findViewById(R.id.tyre19)),(findViewById(R.id.tyre20)),(findViewById(R.id.tyre21)),(findViewById(R.id.tyre22)),(findViewById(R.id.tyre23)),(findViewById(R.id.tyre24)),(findViewById(R.id.tyre25)),(findViewById(R.id.tyre26)),(findViewById(R.id.tyre27)),(findViewById(R.id.tyre28)),(findViewById(R.id.tyre29)),(findViewById(R.id.tyre30)),(findViewById(R.id.tyre31)),(findViewById(R.id.tyre32)),(findViewById(R.id.tyre33)),(findViewById(R.id.tyre34)),(findViewById(R.id.tyre35)),(findViewById(R.id.tyre36)),(findViewById(R.id.tyre37)),(findViewById(R.id.tyre38)),(findViewById(R.id.tyre39)),(findViewById(R.id.tyre40)),(findViewById(R.id.tyre41)),(findViewById(R.id.tyre42)),(findViewById(R.id.tyre43)),(findViewById(R.id.tyre44)),(findViewById(R.id.tyre45)),(findViewById(R.id.tyre46)),(findViewById(R.id.tyre47)),(findViewById(R.id.tyre48)),(findViewById(R.id.tyre49)),(findViewById(R.id.tyre50)),(findViewById(R.id.tyre51)),(findViewById(R.id.tyre52)),(findViewById(R.id.tyre53)),(findViewById(R.id.tyre54)),(findViewById(R.id.tyre55)),(findViewById(R.id.tyre56)),(findViewById(R.id.tyre57)),(findViewById(R.id.tyre58)),(findViewById(R.id.tyre59)),(findViewById(R.id.tyre60)),(findViewById(R.id.tyre61)),(findViewById(R.id.tyre62)),(findViewById(R.id.tyre63))};
+    public TableLayout wheels_table = (findViewById(R.id.wheels_table));
     trailer[] trailers;
     int which_trailer_show;
 
@@ -58,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
         trailer[] trailers = new trailer[6];
         which_trailer_show  = -1;
         show_trailer(which_trailer_show);
+        trailers[1].wheels[10].updateWheel(40,10, trailer.errors.NON);
+        trailers[2].wheels[10].updateWheel(40,10, trailer.errors.NON);
+        trailers[1].wheels[19].updateWheel(40,10, trailer.errors.NON);
+        trailers[1].wheels[30].updateWheel(40,10, trailer.errors.NON);
+        trailers[1].wheels[1].updateWheel(40,10, trailer.errors.NON);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
